3cb2f2f92eec9a730d93d9cc4c8993345f55db23
yii41 <yi.i_41@icloud.com>
2024-06-07 17:19:48 +0900

    add: wikipedia.py

---
+++ week4/wikipedia.py
@@ -0,0 +1,153 @@
+import sys
+import collections
+
+class Wikipedia:
+
+    # Initialize the graph of pages.
+    def __init__(self, pages_file, links_file):
+
+        # A mapping from a page ID (integer) to the page title.
+        # For example, self.titles[1234] returns the title of the page whose
+        # ID is 1234.
+        self.titles = {}
+
+        # A set of page links.
+        # For example, self.links[1234] returns an array of page IDs linked
+        # from the page whose ID is 1234.
+        self.links = {}
+        # Read the pages file into self.titles.
+        with open(pages_file) as file:
+            for line in file:
+                (id, title) = line.rstrip().split(" ")
+                id = int(id)
+                assert not id in self.titles, id
+                self.titles[id] = title
+                self.links[id] = []
+        print("Finished reading %s" % pages_file)
+
+        # Read the links file into self.links.
+        with open(links_file) as file:
+            for line in file:
+                (src, dst) = line.rstrip().split(" ")
+                (src, dst) = (int(src), int(dst))
+                assert src in self.titles, src
+                assert dst in self.titles, dst
+                self.links[src].append(dst)
+        print("Finished reading %s" % links_file)
+        print()
+
+
+    # Find the longest titles. This is not related to a graph algorithm at all
+    # though :)
+    def find_longest_titles(self):
+        titles = sorted(self.titles.values(), key=len, reverse=True)
+        print("The longest titles are:")
+        count = 0
+        index = 0
+        while count < 15 and index < len(titles):
+            if titles[index].find("_") == -1:
+                print(titles[index])
+                count += 1
+            index += 1
+        print()
+
+
+    # Find the most linked pages.
+    def find_most_linked_pages(self):
+        link_count = {}
+        for id in self.titles.keys():
+            link_count[id] = 0
+
+        for id in self.titles.keys():
+            for dst in self.links[id]:
+                link_count[dst] += 1
+
+        print("The most linked pages are:")
+        link_count_max = max(link_count.values())
+        for dst in link_count.keys():
+            if link_count[dst] == link_count_max:
+                print(self.titles[dst], link_count_max)
+        print()
+
+    # Find the shortest path.
+    # |start|: The title of the start page.
+    # |goal|: The title of the goal page.
+
+    def find_shortest_path(self, start, goal):
+        #------------------------#
+        # Write your code here!  #
+
+        # Get id from title
+        start_id = None
+        goal_id = None
+        for id, title in self.titles.items():
+            if title == start:
+                start_id = id
+            if title == goal:
+                goal_id = id
+
+        # BFS
+        deque = collections.deque()
+        visited = {}
+        parent = {}
+        visited[start_id] = True
+        deque.append(start_id)
+
+        while deque:
+            node = deque.popleft()
+            if node == goal_id:
+                path = [goal_id]
+                while path[-1] != start_id:
+                    path.append(parent[path[-1]])
+                path.reverse()
+                print("The shortest path is:")
+                print(" -> ".join(self.titles[node] for node in path))
+                print()
+                return
+            for child in self.links[node]:
+                if child not in visited:
+                    visited[child] = True
+                    parent[child] = node
+                    deque.append(child)
+        #------------------------#
+
+
+    # Calculate the page ranks and print the most popular pages.
+    def find_most_popular_pages(self):
+        #------------------------#
+        pagerank = {page_id: 1 for page_id in self.titles}
+        num_pages = len(self.titles)
+        convergence_condition = sum(pagerank.values())
+
+        while convergence_condition >= 0.01:
+            new_pagerank = {} 
+            for id in self.titles:
+                pagerank_sum = sum(pagerank[src] / len(dst) for src, dst in self.links.items() if id in dst)
+                new_pagerank[id] = 0.15 / num_pages + 0.85 * pagerank_sum
+            convergence_condition = sum((new_pagerank[id] - pagerank[id]) ** 2 for id in pagerank)
+            pagerank = new_pagerank
+
+        sorted_pages = sorted(pagerank.items(), key=lambda x: x[1], reverse=True)
+        for page, rank in sorted_pages:
+            print(page, ":", rank)
+        #------------------------#
+
+
+    # Do something more interesting!!
+    def find_something_more_interesting(self):
+        #------------------------#
+        # Write your code here!  #
+        #------------------------#
+        pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: %s pages_file links_file" % sys.argv[0])
+        exit(1)
+
+    wikipedia = Wikipedia(sys.argv[1], sys.argv[2])
+    wikipedia.find_longest_titles()
+    wikipedia.find_most_linked_pages()
+    wikipedia.find_shortest_path("A", "C")
+    wikipedia.find_most_popular_pages()
